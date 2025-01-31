14390da1e19d80c629e047a81ff524dab4145524
Prakharper <158788969+Prakharper@users.noreply.github.com>
2025-01-30 21:12:58 -0400

    Actualizar info-grupos.js

--- plugins/info-grupos.js
+++ plugins/info-grupos.js
@@ -4,20 +4,20 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 let img = await (await fetch(`https://files.catbox.moe/95lbkz.jpg`)).buffer()
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
-let txt = `*Hola!, te invito a unirte a los grupos oficiales de del Bot para convivir con la comunidad :D*
+let txt = `*Hola!, te invito a unirte a los grupos oficiales de del Bot para convivir con la comunidad.*
 
 1- Demon bot-2.2🩸
-*✰* https://whatsapp.com/channel/0029Vb03dv95a23v1gG0gT24
+*✞︎* https://whatsapp.com/channel/0029Vb03dv95a23v1gG0gT24
 
 *─ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ*
 
 ➠ Enlace anulado? entre aquí! 
 
-♡ Canal Demon :
-*✰* https://whatsapp.com/channel/0029Vb03dv95a23v1gG0gT24
+☠︎︎ Canal Demon :
+*✞︎* https://whatsapp.com/channel/0029Vb03dv95a23v1gG0gT24
 
-♡ Canal Demon :
-*✰* https://whatsapp.com/channel/0029Vb03dv95a23v1gG0gT24
+☠︎︎ Canal Demon :
+*✞︎* https://whatsapp.com/channel/0029Vb03dv95a23v1gG0gT24
 
 > ${dev}`
 await conn.sendFile(m.chat, img, "Thumbnail.jpg", txt, m, null, fake)
