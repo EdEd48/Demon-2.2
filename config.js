f9d99db6e5f99ae522e80e2cb5162e87f6be2d39
Prakharper <158788969+Prakharper@users.noreply.github.com>
2025-03-13 13:51:28 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ global.confirmCode = ''
 global.owner = [
    ['573143681055', 'Creador 👑', true],
    ['50557865603', 'Owner 💛', true],
-   ['', 'Owner ☁️', true],
+   ['584241836217', 'Owner ☁️', true],
    ['', 'Owner 🍭', true],
    [''],
    [''],
